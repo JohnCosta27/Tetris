7c3626f9a7f0a6d186116417af81497f74758281
JohnCosta27 <johncosta027@gmail.com>
2021-01-11 14:53:44 +0000

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World");
+        VBox root = new VBox();
+        primaryStage.setScene(new Scene(root));
+        Label hello = new Label("Hello");
+        Label world = new Label("World");
+        root.getChildren().addAll(hello, world);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        launch(args);
+    }
+}
+//Hello
