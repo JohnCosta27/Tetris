920ef326f99da6260625339f35cbebe6ac38e620
JohnCosta27 <johncosta027@gmail.com>
2021-01-13 19:40:51 +0000

    Shape class

--- src/com/company/Shape.java
+++ src/com/company/Shape.java
@@ -2,8 +2,11 @@ package com.company;
 
 public enum Shape {
 
-    //L_SHAPE(new int[][]{{0, 0, 0}, {0, 0, 0}});
-    L_SHAPE (new String[] (""))
-    private int [][] configuration;
+    L_SHAPE(new int[][] {{0, 0, 0}, {1, 1, 1}});
+    private int[][] configure;
+
+    private Shape(int[][] configure) {
+        this.configure = configure;
+    }
 
 }
