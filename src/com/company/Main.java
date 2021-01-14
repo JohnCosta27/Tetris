212228527bb3329d8c61d1ff934ba63f923cc049
JohnCosta27 <johncosta027@gmail.com>
2021-01-14 12:19:55 +0000

    Committed new Main file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,18 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-publcu 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Scanner;
+
 
 public class Main extends Application {
 
 	static int[][] grid = new int[20][10];
-	
+
+
     public void start(Stage primaryStage) {
+
         primaryStage.setTitle("Hello World");
         VBox root = new VBox();
         primaryStage.setScene(new Scene(root));
@@ -20,44 +25,15 @@ public class Main extends Application {
         Label world = new Label("World");
         root.getChildren().addAll(hello, world);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
 	//write your code here
         // launch(args);
 
-    	
-    	insertShape(Shape.L_SHAPE);
-    	updateShape()
-		L_SHAPE(new int[][] {{0, 0, 1}, {1, 1, 1}});
-    	
-    	printArray(grid);
-    	
+        Game game = new Game();
+
     }
 
-    public static void printArray(int[][] grid) {
-    	for (int i = 0; i < grid.length; i++) {
-    		for (int j = 0; j < grid[0].length; j++) {
-    			System.out.print(grid[i][j]);
-    		}
-    		System.out.println();
-    	}
-    }
-    
-    public static void insertShape(Shape shape) {
-    	int [][] configuration = shape.getConfiguration();
-
-    	int x = shape.getX();
-    	int y = shape.getY();
-
-    	for (int i = 0; i < configuration.length; i++) {
-    		for (int j = 0; j < configuration[0].length; j++) {
-    			
-    			if (configuration[i][j] == 1) grid[y + i][x + j] = configuration[i][j];
-    			
-    		}
-    	}
-    	
-    }
-    
 }
